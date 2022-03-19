d2f9ebbd25631ebe1f359ecb47a0d8ac771d2d42
Anacarolinar21 <anacarolinarodriguescarolina2@gmail.com>
2022-03-18 21:54:06 -0300

    finalizar cebolinha

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// string onde vc classifica uma variavel
+string frase;
+
+Console.WriteLine("Por favor, escreva uma frase");
+frase = Console.ReadLine()!;
+
+string novafrase = frase
+.Replace("r", "l"!)
+.Replace("R","L"!);
+
+Console.WriteLine(novafrase);
